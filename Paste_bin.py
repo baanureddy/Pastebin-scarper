91878e0ed4bbe35fcf67c39b1b9a836fb39b1399
baanureddy <38579753+baanureddy@users.noreply.github.com>
2022-12-29 09:32:40 +0530

    Update Paste_bin.py

--- Paste_bin.py
+++ Paste_bin.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 pattern = r'/[a-zA-Z0-9]{8}'
 url = 'https://pastebin.com/search?q=hotstar&sort=-date'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
-cookies = {'_identity-frontend': '4d86740c4b73d87eb236d5defa4d902e0dc4da933dc21ebad817b7f4d2f18c69a%3A2%3A%7Bi%3A0%3Bs%3A18%3A%22_identity-frontend%22%3Bi%3A1%3Bs%3A53%3A%22%5B9311630%2C%22k8zpYILS9cHp5xnoXIQva5pxu2j7dk6N%22%2C15552000%5D%22%3B%7D','__viCookieActive': 'true'}
+cookies = {'_identity-frontend': '<session-cookie>','__viCookieActive': 'true'}
 
 response = requests.get(url, headers=headers, cookies=cookies).text
 soup = BeautifulSoup(response, 'html.parser')
